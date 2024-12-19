b7c379d57af7644bc9b20cdd98eebc99a6d43ba6
woovoltt <ddcy1111@naver.com>
2024-12-19 20:03:23 +0900

    채팅

---
+++ src/main/java/com/DogProject/domain/ChatMessage.java
@@ -0,0 +1,50 @@
+package com.DogProject.domain;
+
+public class ChatMessage {
+    private String roomId;
+    private Long senderId;
+    private String senderName;
+    private String content;
+    private String time;
+
+    // Getters and Setters
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public Long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Long senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
